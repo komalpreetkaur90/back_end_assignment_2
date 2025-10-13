084971dbcb7c46e66772f16cd1433a279d621adc
Komalpreet kaur <kkaur209@academic.rrc.ca>
2025-10-12 21:38:22 -0500

    Corrected the mistakes from assignment 2

--- src/app.ts
+++ src/app.ts
@@ -8,20 +8,16 @@ import branchRoutes from "./api/v1/routes/branchRoutes";
 const app: Express = express();
 
 // Use morgan for HTTP request logging
+app.use(express.json());
 app.use(morgan("combined"));
+
 
 // Health check route
 app.get("/health", (req, res) => {
     res.status(200).send("Server is healthy");
 });
 
-app.use("/api/v1", employeeRoutes);
-app.use("/api/v1", branchRoutes);
-
-const PORT: string | number = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.use("/api/v1/employees", employeeRoutes);
+app.use("/api/v1/branches", branchRoutes);
 
 export default app;
